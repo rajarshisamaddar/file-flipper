704b2c7301dcb35f45dc048cbb7ddf80d6358485
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-02-11 22:42:30 +0530

    production v1.0

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,109 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="space-y-8 text-md md:text-lg pb-4 md:pb-8">
+      <p className="text-sm">Effective Date: Sat 10 Feb 2024</p>
+      <p>
+        Welcome to FileFlipper's Privacy Policy. Your privacy is a top priority
+        for me. This policy outlines how I gather, utilize, and secure your
+        personal information when you engage with my website and services.
+        Please take a moment to review this document to understand my practices.
+      </p>
+
+      <div className="space-y-4">
+        <h2 className="text-xl md:text-2xl font-medium">
+          1. Information Collection
+        </h2>
+        <p>
+          I collect limited data through Google Analytics to enhance your user
+          experience. This includes information like visited pages, IP address,
+          browser type, and device details. The collected data is instrumental
+          in refining my website's functionality and content.
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <h2 className="text-xl md:text-2xl font-medium">
+          2. Use of Your Information
+        </h2>
+        <p>
+          Information gathered via Google Analytics is strictly utilized to
+          comprehend user behavior and optimize my website's performance. Your
+          data is never sold, rented, or shared with third parties, ensuring
+          your privacy is maintained.
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <h2 className="text-xl md:text-2xl font-medium">
+          3. Cookies and Tracking
+        </h2>
+        <p>
+          To improve your interactions with my website, I employ cookies and
+          tracking technologies. You have the ability to manage your cookie
+          preferences in your browser settings; however, disabling cookies may
+          impact your overall experience.
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <h2 className="text-xl md:text-2xl font-medium">4. Data Security</h2>
+        <p>
+          I implement reasonable measures to safeguard your data. It's important
+          to note that while I strive for the highest level of security, no
+          method of internet transmission is entirely foolproof.
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <h2 className="text-xl md:text-2xl font-medium">
+          5. Third-Party Links
+        </h2>
+        <p>
+          My website may feature links to third-party sites. I do not control
+          their content or privacy policies. I recommend reviewing their
+          policies before providing any personal information on these external
+          sites.
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <h2 className="text-xl md:text-2xl font-medium">
+          6. Children's Privacy
+        </h2>
+        <p>
+          FileFlipper is not designed for users under the age of 13. If you
+          believe a child has shared personal information, please contact me for
+          appropriate action.
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <h2 className="text-xl md:text-2xl font-medium">
+          7. Changes to This Policy
+        </h2>
+        <p>
+          I may revise this Privacy Policy without prior notice. Any changes
+          take effect immediately upon posting. Please check the revision date
+          at the top for the most recent version.
+        </p>
+      </div>
+
+      <div className="space-y-4">
+        <h2 className="text-xl md:text-2xl font-medium">8. Contact Me</h2>
+        <p>
+          If you have questions or concerns, feel free to reach out to me at{" "}
+          <a className="text-primary" href="mailto:itsrajarshi@gmail.com">
+            itsrajarshi@gmail.com
+          </a>
+          .
+        </p>
+      </div>
+
+      <p>
+        By utilizing FileFlipper, you agree to abide by this Privacy Policy. If
+        you do not agree, please discontinue using my services. I appreciate
+        your trust in me with your privacy.
+      </p>
+    </div>
+  );
+}
